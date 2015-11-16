c8b743dc4a5c8f3695725b5e3d178bcf6da0670e
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-11-16 16:05:32 -0600

    a loop I play with my ole proteus/2

---
+++ opus2.py
@@ -0,0 +1,63 @@
+from pprint import pprint
+from time import sleep
+import mido
+import random
+import mingus.core.scales as scales
+from mingus.containers import Note
+
+output = mido.open_output( u'FCA1616 MIDI 1')
+
+loop = [
+    [1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 1, 0],
+    [1, 0, 1, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 0, 0, 1],
+    [0, 1, 0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0, 0],
+    ]
+
+# C3 = 48
+escala = scales.HarmonicMajor('D')
+escala.octaves = 1
+notas = []
+for o in range(5,8):
+    for n in escala.ascending():
+        notas.append(int(Note(n, o)))
+
+pprint(notas)
+bpm = 111
+delay = 60.0 / bpm
+
+try:
+    t = 0
+    while True:
+        sleep(delay)
+        
+        for i in range(0,len(loop[t])):
+            if loop[t][i]:
+                if not loop[t-1][i]:
+                    output.send(mido.Message('note_on', note=notas[i], velocity=64))
+            else:
+                if loop[t-1][i]:
+                    output.send(mido.Message('note_off', note=notas[i], velocity=64))
+
+        print loop[t]
+
+        # as time goes by
+        t+=1
+        if t == len(loop):
+            t = 0
+        
+
+except KeyboardInterrupt:
+    for i in range(0,len(loop[0])):
+        output.send(mido.Message('note_off', note=notas[i], velocity=64))
+    output.close()
+
+output.close()
