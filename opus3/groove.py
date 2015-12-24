4720e01b4be36ade5f04cd3cceea631c5e18fb77
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-12-23 20:06:10 -0600

    create loop objects from CAs

---
+++ opus3/groove.py
@@ -0,0 +1,18 @@
+from automata import TransitionAutomata, ShadeAutomata
+import itertools
+from pprint import pprint
+
+class Groove:
+
+    def __init__(self, length=1, repeat=2, transitions=7, staccato=6):
+        
+        ta = TransitionAutomata()
+        sa = ShadeAutomata()
+
+        self.loop = []
+        
+        subgroove = ta.get_groove(transitions, length)
+        epa = [subgroove for r in range(repeat)]
+        groove = itertools.chain( *epa )
+        self.loop = [[int(n),] for n in groove]
+        
---
+++ opus3/test_groove.py
@@ -0,0 +1,6 @@
+from groove import Groove
+from pprint import pprint
+
+g = Groove(transitions=4, length=4, repeat=1)
+
+pprint(g.loop)
