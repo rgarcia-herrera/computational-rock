a568eff6d0fdb9ffa7fd1c7c0a71ab0b90933a1d
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-12-31 17:37:28 -0600

    try different networks, scales, nice bass lines!

--- opus3/test_melody.py
+++ opus3/test_melody.py
@@ -5,23 +5,27 @@ from mingus.containers import Note
 import networkx as nx
 import random
 from pprint import pprint
-
+from groove import Roll
 from melody import Melody, scale2ints, random_walk_edge_weight
+from itertools import combinations
 
 # configure scale as list of ints: midi notes
-scale = scale2ints(scales.Dorian, 'A', span=2, octave=3)
+scale = scale2ints(scales.Major, 'A', span=1, octave=2)
 
 
 # prepare network of intervals
-g = nx.barabasi_albert_graph(len(scale), 2)
+g = nx.barabasi_albert_graph(len(scale), 4)
+#g = nx.erdos_renyi_graph(len(scale),0.2)
+#g = nx.watts_strogatz_graph(len(scale), 3, 0.3)
 random_walk_edge_weight(g)
+
 
 
 # create roll object
 roll = Roll(length      = 1,
-            repeat      = 2,
-            transitions = 7,
-            shade       = 6)
+            repeat      = 4,
+            transitions = 9,
+            shade       = 2)
 
 # bring it all together
 m = Melody(scale=scale,
@@ -30,9 +34,10 @@ m = Melody(scale=scale,
            repeat=2,
            roll=roll)
 
+print m.loop[0]
 # loop = Melody.loop
-# output = mido.open_output( u'ZynAddSubFX')
-# s = Sequencer( output, bpm=40, loop=loop)
+output = mido.open_output( u'ZynAddSubFX')
+s = Sequencer( output, bpm=120, loop=m.loop)
 
-# s.play()
+s.play()
 
