ef8c324d9824a1263b641334ac042a2d5d782e7a
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-12-31 03:40:51 -0600

    usage of scale, graph and roll

--- opus3/test_melody.py
+++ opus3/test_melody.py
@@ -8,10 +8,27 @@ from pprint import pprint
 
 from melody import Melody, scale2ints, random_walk_edge_weight
 
+# configure scale as list of ints: midi notes
 scale = scale2ints(scales.Dorian, 'A', span=2, octave=3)
+
+
+# prepare network of intervals
 g = nx.barabasi_albert_graph(len(scale), 2)
 random_walk_edge_weight(g)
-m = Melody(scale=scale, graph=g, length=1, repeat=2, transitions=7, )
+
+
+# create roll object
+roll = Roll(length      = 1,
+            repeat      = 2,
+            transitions = 7,
+            shade       = 6)
+
+# bring it all together
+m = Melody(scale=scale,
+           graph=g,
+           length=1,
+           repeat=2,
+           roll=roll)
 
 # loop = Melody.loop
 # output = mido.open_output( u'ZynAddSubFX')
