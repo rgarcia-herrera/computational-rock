bc4e35be14b55ce636ca09aac7b17a7e62f5f93b
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-12-30 18:31:28 -0600

    this should test the melody module

--- opus3/test_melody.py
+++ opus3/test_melody.py
@@ -10,10 +10,12 @@ from melody import Melody, scale2ints
 
 scale = scale2ints(scales.Dorian, 'A', span=2, octave=3)
 g = nx.barabasi_albert_graph(len(scale), 2)
-m = Melody(scale=scale, graph=g, length=1, repeat=2, transitions=7, ):
-loop = Melody.loop
-output = mido.open_output( u'ZynAddSubFX')
-s = Sequencer( output, bpm=40, loop=loop)
 
-s.play()
+m = Melody(scale=scale, graph=g, length=1, repeat=2, transitions=7, )
+
+# loop = Melody.loop
+# output = mido.open_output( u'ZynAddSubFX')
+# s = Sequencer( output, bpm=40, loop=loop)
+
+# s.play()
 
