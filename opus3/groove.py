ccb9ee000a9fcb8153668aa0d104f8f2b0b8203a
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-12-31 03:39:39 -0600

    groove is part of a roll

--- opus3/groove.py
+++ opus3/groove.py
@@ -2,17 +2,20 @@ from automata import TransitionAutomata, ShadeAutomata
 import itertools
 from pprint import pprint
 
-class Groove:
+class Roll:
 
-    def __init__(self, length=1, repeat=2, transitions=7, staccato=6):
+    def __init__(self, length=1, repeat=2, transitions=7, shade=6):
         
         ta = TransitionAutomata()
-        sa = ShadeAutomata()
-
-        self.loop = []
         
         subgroove = ta.get_groove(transitions, length)
         epa = [subgroove for r in range(repeat)]
         groove = itertools.chain( *epa )
-        self.loop = [int(n) for n in groove]
+        self.groove = [int(n) for n in groove]
+
+        sa = ShadeAutomata()
+        
+        subgroove = sa.get_groove(shade, length)
+        groove = itertools.chain( *[subgroove for r in range(repeat)] )
+        self.staccato = [int(n) for n in groove]
         
