a0adff6834a5cabebd8f4ef7a7a9df2bd983a3b5
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-12-31 17:37:00 -0600

    render pianoroll with melody

--- opus3/melody.py
+++ opus3/melody.py
@@ -2,7 +2,7 @@ import mingus.core.scales as scales
 from mingus.containers import Note
 from automata import TransitionAutomata, ShadeAutomata
 import itertools
-from groove import Groove
+from groove import Roll
 import networkx as nx
 import random
 from pprint import pprint
@@ -50,6 +50,8 @@ class Melody:
             w = self.graph.get_edge_data(node,m)['w']
             for i in range(w):
                 choices.append(m)
+                if random.choice([True, False]):
+                    choices.append(node)
         return random.choice(choices)
 
     
@@ -61,12 +63,29 @@ class Melody:
         self.loop  = [ scale, ]
 
         submelody = 8
-        note = scale[0]
-        for tine in self.roll.groove:
-            # render note
+        note = 0
+        for i in range(len(self.roll.groove)):
+                       
+            # render sixteenth
+            t = []
+            for n in range(len(scale)):
+                if self.roll.groove[i] and n == note:
+                    t.append(1)
+                else:
+                    t.append(0)
 
+            s = []
+            for n in range(len(scale)):
+                if self.roll.staccato[i] and n == note:
+                    s.append(1)
+                else:
+                    s.append(0)
+                    
+            self.loop.append((t,s))
             # get a new note
-            note = self.random_walk_interval( note )
+            if i < len(self.roll.groove)-1:
+                if self.roll.groove[i] == 1 and self.roll.groove[i+1] == 0:
+                    note = self.random_walk_interval( note )
         
         
 
